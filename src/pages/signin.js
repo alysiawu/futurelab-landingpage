b51a423d0734baba36e5ae082f5275975f161724
Alysia Wu <alysiawu@Alysias-MacBook-Pro.local>
2022-05-25 12:27:24 -0700

    signup

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -4,8 +4,10 @@ const SigninPage = () => {
   return (
     <div>
 
+      <p align="center">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScG7hMIai9azxKSm0gv9bPNdNDy4COnohAz-ouy34jjjQvZAQ/viewform?embedded=true" width="640" height="640" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScG7hMIai9azxKSm0gv9bPNdNDy4COnohAz-ouy34jjjQvZAQ/viewform?embedded=true" width="640" height="640" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      </p>
     </div>
   );
 };
